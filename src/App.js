f803de2672f40fd95f8b86610066ae9a1cf16e8c
hoiee04 <chaehl1004@gmail.com>
2024-05-09 10:54:38 +0900

    Footer 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from './components/Header.jsx';
 import BoxofficeRank from './components/BoxofficeRank.jsx';
+import Footer from './components/Footer.jsx';
+
 function App() {
   return (
     <>
       <Header />
       <BoxofficeRank />
+      <Footer />
     </>
   );
 }
